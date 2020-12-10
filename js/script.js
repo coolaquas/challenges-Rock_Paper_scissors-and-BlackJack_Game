862fc3d2d824c93d1a68e2b046db40d694abd955
coolaquas <samrat.mallick1@gmail.com>
2020-12-10 23:01:01 +0530

    New thing

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ $(".btn-primary").click(() => {
     let birthyear = prompt("Please enter your birth year");
     let ageInDays = (2020 - birthyear) * 365;
     if( ageInDays != null){
-        $(".flex-box-result").text(ageInDays +" Day's you have already lived");
+        $(".flex-box-result").html(`<h1>${ageInDays} Day's you have already lived</h1>`);
     }
 })
