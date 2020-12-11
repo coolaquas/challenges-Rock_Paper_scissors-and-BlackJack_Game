b7dbd02382d758f019a71f3ecfe93b24e033a6d6
coolaquas <samrat.mallick1@gmail.com>
2020-12-11 10:34:48 +0530

    challenge 2 completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+$("document").ready(()=> {
+    $(".flex-box-result").hide(); 
+})
 $(".btn-primary").click(() => {
     let birthyear = prompt("Please enter your birth year");
     let ageInDays = (2020 - birthyear) * 365;
-    if( ageInDays != null){
-        $(".flex-box-result").html(`<h1>${ageInDays} Day's you have already lived</h1>`);
-    }
+        $(".flex-box-result").show().html(`<h1>hey!you have already lived ${ageInDays} Day's </h1>`);
 })
+$(".btn-danger").click(() => {
+    $(".flex-box-result").hide();
+})
+var img_gen = ()=> {
+    let image = document.createElement("img");
+    let div = $(".flex-box-container-2");
+    image.src = "https://source.unsplash.com/random/150x100";
+    div.append(image);
+};
+$("#gen_btn").click(img_gen);
+
