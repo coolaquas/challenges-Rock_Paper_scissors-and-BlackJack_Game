6dfd6c516a6104f1a11c374f4556406dac0292e9
coolaquas <samrat.mallick1@gmail.com>
2020-12-13 11:59:55 +0530

    Challenge 5 Completed

--- js/script.js
+++ js/script.js
@@ -129,18 +129,29 @@ let blackjackGame = {
     'dealer': { 'scorespan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
     'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
     'cardsMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11] },
+    'wins': 0,
+    'losses': 0,
+    'draws': 0,
+    'isStand': false,
+    'turnsOver': false,
 }
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
+const hitSound = new Audio("../public/sounds/swish.m4a");
+const winSound = new Audio("../public/sounds/cash.mp3");
+const lostSound = new Audio("../public/sounds/aww.mp3");
+
+
 let randCard = () => {
     return blackjackGame.cards[(Math.floor(Math.random() * 13))]
 }
 let showCard = (activePlayer, card) => {
-    let cardImage = document.createElement("img");
-    cardImage.src = `/public/images/${card}.png`;
-    const hitSound = new Audio("../public/sounds/swish.m4a");
-    hitSound.play();
-    $(activePlayer.div).append(cardImage);
+    if (activePlayer.score <= 21) {
+        let cardImage = document.createElement("img");
+        cardImage.src = `/public/images/${card}.png`;
+        hitSound.play();
+        $(activePlayer.div).append(cardImage);
+    }
 }
 let updateScore = (activePlayer, card) => {
     if (card == 'A') {
@@ -152,25 +163,98 @@ let updateScore = (activePlayer, card) => {
     } else {
         activePlayer.score += blackjackGame.cardsMap[card];
     }
-
-    $(activePlayer.scorespan).text(activePlayer.score);
 }
-$("#blackjack-hit-button").click(() => {
-    let randomCard = randCard();
-    showCard(DEALER, randomCard);
-    updateScore(DEALER, randomCard);
-    console.log(DEALER.score);
+let showScore = (activePlayer) => {
+    if (activePlayer.score > 21) {
+        $(activePlayer.scorespan).text("BUST!").css("color", "red");
+    } else {
+        $(activePlayer.scorespan).text(activePlayer.score);
+    }
+}
+let sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+async function dealerLogic() {
+    blackjackGame.isStand = true;
+    while (DEALER.score < 16 && blackjackGame.isStand === true) {
+        let card = randCard();
+        showCard(DEALER, card);
+        updateScore(DEALER, card);
+        showScore(DEALER);
+        await sleep(1000);
+    }
+    blackjackGame.turnsOver = true;
+    let winner = computeWiner();
+    showResult(winner);
+}
+
+let computeWiner = () => {
+    let winner;
+    if (YOU.score <= 21) {
+        if ((YOU.score > DEALER.score) || (DEALER.score > 21)) {
+            blackjackGame.wins++;
+            winner = YOU;
+        } else if (YOU.score < DEALER.score) {
+            blackjackGame.losses++;
+            winner = DEALER;
+        } else if (YOU.score === DEALER.score) {
+            blackjackGame.draws++;
+            console.log("You Drew");
+        }
+    } else if (YOU.score > 21 && DEALER.score <= 21) {
+        blackjackGame.losses++;
+        winner = DEALER;
+    } else if (YOU.score > 21 && DEALER.score > 21) {
+        blackjackGame.draws++;
+        console.log("You Drew");
+    }
+    return winner;
+}
+
+let showResult = (winner) => {
+    let message, messageColor;
+    if (winner === YOU) {
+        message = "You Won!";
+        messageColor = "green";
+        winSound.play();
+    } else if (winner === DEALER) {
+        message = "You Lost!";
+        messageColor = "red";
+        lostSound.play();
+    } else {
+        message = "You Draw!";
+        messageColor = "yellow";
+    }
+    $("#blackjack-result").text(message).css('color', messageColor);
+    $("#wins").text(blackjackGame.wins);
+    $("#looses").text(blackjackGame.losses);
+    $("#draws").text(blackjackGame.draws);
+
+}
+
+$("#blackjack-hit-button").click(() => {
+    if (!blackjackGame.isStand) {
+        let randomCard = randCard();
+        showCard(YOU, randomCard);
+        updateScore(YOU, randomCard);
+        showScore(YOU);
+    }
 })
 $("#blackjack-stand-button").click(() => {
-
+    if(!blackjackGame.turnsOver){
+        dealerLogic();
+    }
 })
 $("#blackjack-deal-button").click(() => {
-    $(".flex-blackjack-row-1 img").remove();
-    $("#blackjack-result").text("Let's Play");
-    $("#your-blackjack-result").text(0);
-    $("#dealer-blackjack-result").text(0);
-    YOU.score = 0;
-    DEALER.score = 0;
-    console.log(DEALER.score);
+    if (blackjackGame.turnsOver) {
+        blackjackGame.isStand = false;
+        blackjackGame.turnsOver = false;
+        $(".flex-blackjack-row-1 img").remove();
+        $("#blackjack-result").text("Let's Play").css('color', '#000');
+        $("#your-blackjack-result").text(0).css("color", '#fff');
+        $("#dealer-blackjack-result").text(0).css("color", '#fff');
+        YOU.score = 0;
+        DEALER.score = 0;
+    }
 })
